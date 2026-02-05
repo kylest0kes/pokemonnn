63dc00ff5fd423d9a4c7803cdd9d835f774c4276
kylest0kes <kylestokes1551@gmail.com>
2026-02-05 18:52:34 -0500

    created pokemon dto

--- pokemonnn-backend/src/main/java/com/example/pokemonnn_backend/dto/PokemonDTO.java
+++ pokemonnn-backend/src/main/java/com/example/pokemonnn_backend/dto/PokemonDTO.java
@@ -1,5 +1,38 @@
 package com.example.pokemonnn_backend.dto;
 
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PokemonDTO {
+
+    private int id;
+
+    private String name;
+
+    private String type;
+
+    private int weight;
+
+    private int height;
+
+    private List<String> abilities;
+
+    private List<String> characteristics;
+
+    private String sprite;
+    
+    private String color;
+
+    private String shape;
+
+    private String species;
+
+    private List<String> locations;
+
+    private String gender;
     
 }
