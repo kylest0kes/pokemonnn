14ae33617efbd4f4c29029da02cefb8edac84616
kylest0kes <kylestokes1551@gmail.com>
2026-02-17 18:30:10 -0500

    did more frontend stuff

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Grid } from "./shared/grid/grid";
 import { Header } from "./shared/header/header";
+import { Navbar } from "./shared/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- frontend/src/app/shared/navbar/navbar.ts
+++ frontend/src/app/shared/navbar/navbar.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  searchBy: 'name' | 'type' = 'name';
+  pokemonTypes: string[] = ['normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy', 'stellar', 'unknown'];
+
+  // RE_IMPLEMENT WITH BACKEND API LATER!!!!!!!!!!!
+  // ngOnInit() {
+  //   this.loadPokemonTypes();
+  // }
+
+  onSearchChange() {
+    // if (this.searchBy === 'type' && this.pokemonTypes.length === 0) {
+    //   this.loadPokemonTypes();
+    // }
+  }
+
+  // loadPokemonTypes() {
+  //   fetch('https://pokeapi.co/v2/type')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       this.pokemonTypes = data.results.map((t: any) => t.name);
+  //     })
+  // }
 
 }
