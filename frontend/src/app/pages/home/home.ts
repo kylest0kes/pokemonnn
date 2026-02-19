d680eeaf52cb044400cf098adf43eaa30fd89a98
kylest0kes <kylestokes1551@gmail.com>
2026-02-19 17:40:24 -0500

    got data flowing from home component to grid

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -2,32 +2,22 @@ import { Component } from '@angular/core';
 import { Grid } from "../../shared/grid/grid";
 import { PokemonDTO } from '../../models/pokemon-dto.interface';
 import { PokemonService } from '../../services/PokemonService';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [Grid],
+  imports: [Grid, AsyncPipe],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  pokemon: PokemonDTO[] = [];
+  pokemon$?: Observable<PokemonDTO[]>;
 
   constructor(private pokemonService: PokemonService,) {}
 
   ngOnInit() {
-    this.loadPokemon();
+    this.pokemon$ = this.pokemonService.getAllPokemon();
+    console.log("home pokemon: ", this.pokemon$);
   }
-
-  loadPokemon() {
-    this.pokemonService.getAllPokemon()
-      .subscribe({
-        next: (data) => {
-          console.log("pokemon data: ", data);
-        },
-        error: (err) => {
-          console.error("Get All Pokemon Error: ", err);
-        }
-      })
-  }
-
 }
--- frontend/src/app/shared/grid/grid.ts
+++ frontend/src/app/shared/grid/grid.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { PokemonDTO } from '../../models/pokemon-dto.interface';
+import { Observable } from 'rxjs';
 
 @Component({
+  standalone: true,
   selector: 'app-grid',
   imports: [],
   templateUrl: './grid.html',
   styleUrl: './grid.scss',
 })
-export class Grid {
+export class Grid implements OnChanges{
+  @Input() pokemon: PokemonDTO[] | null = null;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['pokemon'] && this.pokemon) {
+      console.log("pokemon data flowing from home: ", this.pokemon);
+    }
+  }
 
 }
