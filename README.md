4de706564fd53db13b60ae999a3b0cee1535ecdd
kylest0kes <kylestokes1551@gmail.com>
2026-03-03 14:14:40 -0500

    nav to pokemon details page, data is passed to the details page, and data can be seen on page

--- README.md
+++ README.md
@@ -4,5 +4,3 @@ fun stuff
 
 # TODO
 - make a details page layout
-- when clicking a card on homepage it takes you to details page for clicked pokemon
-- implement 'loading' state with loading icon while loading data from api on details page
--- frontend/src/app/pages/pokemonpage/pokemonpage.ts
+++ frontend/src/app/pages/pokemonpage/pokemonpage.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule, TitleCasePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonService } from '../../services/pokemon.service.';
+import { PokemonDTO } from '../../models/pokemon-dto.interface';
 
 @Component({
   standalone: true,
   selector: 'app-pokemonpage',
-  imports: [],
+  imports: [TitleCasePipe, CommonModule, HttpClientModule],
   templateUrl: './pokemonpage.html',
   styleUrl: './pokemonpage.scss',
 })
-export class Pokemonpage {
+export class Pokemonpage implements OnInit {
+  pokemon: PokemonDTO | null = null;
+  isLoading: boolean = true;
 
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    // Get the 'name' parameter from the route
+    this.route.paramMap.subscribe(params => {
+      const name = params.get('name');
+      console.log("param: ", name);
+      if (name) {
+        this.pokemonService.getPokemonByName(name).subscribe({
+          next: (pokemon) => {
+            console.log("pokemon data: ", pokemon);
+            this.pokemon = pokemon;
+            this.isLoading = false;
+            this.cdr.detectChanges();
+          },
+          error: (err) => {
+            console.log("error: ", err)
+            this.isLoading = false;
+            this.cdr.detectChanges();
+          }
+        });
+      }
+    });
+  }
 }
